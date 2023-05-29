f0be224399aed86ebfa086fa5fe1946c9f39d8e4
Jtownokie <6673@holbertonstudents.com>
2023-05-28 21:28:12 -0700

    Combo putchar

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int zn;
+	int comma = 44;
+	int space = 32;
+
+	for (zn = 48; zn <= 57; zn++)
+	{
+		putchar(zn);
+		putchar(comma);
+		putchar(space);
+	}
+
+	return (0);
+}
