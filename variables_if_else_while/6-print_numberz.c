81e58c7616e688fcb1f81f70493d5d0b04b2548f
Jtownokie <6673@holbertonstudents.com>
2023-05-28 21:15:29 -0700

    0-9 putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int zn;
+
+	for (zn = 48; zn <= 57; zn++)
+	{
+		putchar(zn);
+	}
+	putchar('\n');
+
+	return (0);
+}
