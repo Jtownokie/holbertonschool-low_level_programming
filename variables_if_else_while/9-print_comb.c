fc0a7bc86e75de8f089656f333597ef19c0d0453
Jtownokie <6673@holbertonstudents.com>
2023-05-28 21:30:24 -0700

    Fix 1

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -11,13 +11,15 @@ int main(void)
 	int zn;
 	int comma = 44;
 	int space = 32;
+	int nine = 57;
 
-	for (zn = 48; zn <= 57; zn++)
+	for (zn = 48; zn <= 56; zn++)
 	{
 		putchar(zn);
 		putchar(comma);
 		putchar(space);
 	}
+	putchar(nine);
 
 	return (0);
 }
