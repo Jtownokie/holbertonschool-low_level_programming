fe9e63db4e0b7ed56ad77f0348101a7b76f74135
Jtownokie <6673@holbertonstudents.com>
2023-05-29 19:32:05 -0700

    Fix 2

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,12 @@
 /**
  * print_alphabet()- Prints lowercase alphabet followed by newline
  *
- * Description: Prints lowercase alphabet followed by newline
+ * Return: Always 0
  */
 void print_alphabet(void)
 {
 	char ch;
+	int _putchar;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
 	{
