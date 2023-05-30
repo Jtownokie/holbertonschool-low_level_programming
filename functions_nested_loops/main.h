5c32405f2badecffd3f6c0e2040c151274d191da
Jtownokie <6673@holbertonstudents.com>
2023-05-29 20:53:24 -0700

    Fix 1

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 /* _putchar prototype */
 int _putchar(char c);
@@ -8,3 +9,6 @@ void print_alphabet(void);
 
 /* Alphabet x10 putchar */
 void print_alphabet_x10(void);
+
+/* isLower function */
+int _islower(int c);
