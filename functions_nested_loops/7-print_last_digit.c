4c56921098a92efe433b4aae5cc2f903605ba004
Jtownokie <6673@holbertonstudents.com>
2023-05-30 17:43:33 -0700

    Fix 4

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,10 +10,12 @@ int print_last_digit(int x)
 {
 	int num;
 	int lastdigit;
+	char lastchar;
 
 	num = x;
 	lastdigit = num % 10;
-	_putchar(lastdigit);
+	lastchar = lastdigit + '0';
+	_putchar(lastchar);
 
 	return (lastdigit);
 }
