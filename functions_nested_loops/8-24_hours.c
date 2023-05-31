e9b189028344d339e73ed05504c88d38b68d0391
Jtownokie <6673@holbertonstudents.com>
2023-05-30 19:32:36 -0700

    Fix 1

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -14,16 +14,16 @@ void jack_bauer(void)
 
 	for (i = 0; i < 3; i++)
 	{
-		_putchar('0' + i);
 		for (j = 0; j < 4; j++)
 		{
-			_putchar('0' + j);
-			_putchar(':');
 			for (k = 0; k < 6; k++)
 			{
-				_putchar('0' + k);
 				for (l = 0; l < 10; l++)
 				{
+					_putchar('0' + i);
+					_putchar('0' + j);
+					_putchar(':');
+					_putchar('0' + k);
 					_putchar('0' + l);
 					_putchar('\n');
 				}
