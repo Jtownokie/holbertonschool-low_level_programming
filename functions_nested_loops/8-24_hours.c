ea83d21f83394968c9c87e0133481a646290270e
Jtownokie <6673@holbertonstudents.com>
2023-05-31 07:59:43 -0700

    fix

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -20,10 +20,11 @@ void jack_bauer(void)
 			{
 				for (l = 0; l < 10; l++)
 				{
-					if ((i == 2) && (j == 4))
+					if ((i == 2) && (j > 3))
 					{
 						break;
 					}
+
 					_putchar('0' + i);
 					_putchar('0' + j);
 					_putchar(':');
