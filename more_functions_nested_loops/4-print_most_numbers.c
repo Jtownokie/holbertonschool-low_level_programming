0c26f9afa80a506112316500c4dd4eea8a4afd2c
Jtownokie <6673@holbertonstudents.com>
2023-05-31 12:13:52 -0700

    Print Numbers ! 2 and 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints 0-9 except for 2 and 4
+ *
+ * Return: Always void
+ */
+void print_most_numbers(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		if (ch != 2 || ch != 4)
+		{
+			_putchar(ch);
+		}
+	}
+	_putchar('\n');
+}
