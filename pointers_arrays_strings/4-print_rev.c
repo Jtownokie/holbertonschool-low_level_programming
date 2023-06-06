3a8acf670fdc9ba6dcbe258d80cca0f188800ac1
Jtownokie <6673@holbertonstudents.com>
2023-06-06 14:55:54 -0700

    fix

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -8,19 +8,18 @@
  */
 void print_rev(char *s)
 {
+	int length = 0;
 	int i;
-	int length = _strlen(s);
-	int tmp;
 
-	for (i = 0; i < length / 2; i ++)
-	{
-		tmp = s[i];
-		s[i] = s[length - i - 1];
-		s[length - i - 1] = tmp;
-	}
 	while (*s != '\0')
 	{
-		_putchar(*s);
-		s++;
+		length = length + 1;
+		*s = *s + 1;
 	}
+
+	for (i = (length - 1); i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
 }
