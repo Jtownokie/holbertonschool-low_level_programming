6d93f38719c48f8ff75545cce6b461051bb4d426
Jtownokie <6673@holbertonstudents.com>
2023-06-05 18:09:50 -0700

    Test

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/* Putchar Function */
+int _putchar(char c);
+
+/* Reset to 98 Function */
+void reset_to_98(int *n);
+
+/* Swap two integers Function */
+void swap_int(int *a, int *b);
+
+/* Length of String Function */
+int _strlen(char *s);
+
+/* Print String Function */
+void _puts(char *str);
+
+/* Print String Reverse Function */
+void print_rev(char *s);
+
+/* Reverse String Function */
+void rev_string(char *s);
+
+/* Print Every Other Function */
+void puts2(char *str);
+
+/* Print Half String Function */
+void puts_half(char *str);
+
+/* Print n Elements Function */
+void print_array(int *a, int n);
+
+/* Strcpy Function */
+char *_strcpy(char *dest, char *src);
+
+/* Convert String to Integer Function */
+int _atoi(char *s);
