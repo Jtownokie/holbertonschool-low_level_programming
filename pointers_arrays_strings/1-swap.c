0690aea372b568e9432def4e752c90b5f0542cfc
Jtownokie <6673@holbertonstudents.com>
2023-06-05 18:14:52 -0700

    Swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - Swaps value of two integers
+ * @a: Pointer Input 1
+ * @b: Pointer Input 2
+ *
+ * Return: Always void
+ */
+void swap_int(int *a, int *b);
+{
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
