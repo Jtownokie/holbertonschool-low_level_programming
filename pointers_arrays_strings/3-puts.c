b6a490d5f247a9dd59c30a82afd9d4f7dfa26858
Jtownokie <6673@holbertonstudents.com>
2023-06-05 18:29:04 -0700

    Print String

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - Print String Function
+ * @str: Pointer input
+ *
+ * Return: Always void
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; i != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+}
