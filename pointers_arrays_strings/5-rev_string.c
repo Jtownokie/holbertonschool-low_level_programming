933fa4b3b177c73b5125ca869b165fe90b9ed4ef
Jtownokie <6673@holbertonstudents.com>
2023-06-07 12:44:35 -0700

    Reverse String Function

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverse String Function
+ * @s: Pointer input
+ *
+ * Return: Always void
+ */
+void rev_string(char *s)
+{
+	char *str;
+	int len = 0;
+	int rev;
+
+	for (str = s[0]; str != '\0'; str++)
+	{
+		len++
+	}
+
+	len = len - 1;
+
+	for (i = len; i >= 0; i--)
+	{
+		for (j = 0; j <= len; j++)
+		{
+			s[j] = s[i];
+		}
+	}
+}
