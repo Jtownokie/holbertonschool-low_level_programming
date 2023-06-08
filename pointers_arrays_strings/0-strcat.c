1361cb1c19289c42c7e310b6c6694c7f910b8c21
Jtownokie <6673@holbertonstudents.com>
2023-06-08 09:53:47 -0700

    Str Cat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * *_strcat - Function that concatenates two strings
+ * @dest: String Pointer Destination
+ * @src: String to Append to Destination
+ *
+ * Return: Pointer to New String
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	while (*dest != '\0')
+	{
+		dest++;
+	}
+	while (*src != '\0')
+	{
+		dest++ = src++;
+	}
+	_putchar('\0');
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -37,3 +37,27 @@ char *_strcpy(char *dest, char *src);
 
 /* Convert String to Integer Function */
 int _atoi(char *s);
+
+/* Strcat Function */
+char *_strcat(char *dest, char *src);
+
+/* Strcat N Bytes Function */
+char *_strncat(char *dest, char *src, int n);
+
+/* Strcpy N Function */
+char *_strncpy(char *dest, char *src, int n);
+
+/* Strcmp Function */
+char _strcmp(char *s1, char *s2);
+
+/* Reverse Array Function */
+void reverse_array(int *a, int n);
+
+/* To Upper Function */
+char *string_toupper(char *);
+
+/* Cap String */
+char *cap_string(char *);
+
+/* Leet Code Function */
+char *leet(char *);
