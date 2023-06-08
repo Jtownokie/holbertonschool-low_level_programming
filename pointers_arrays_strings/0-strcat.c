e939bba1f8972e0d66cda5c1afa99ba88b889e0e
Jtownokie <6673@holbertonstudents.com>
2023-06-08 09:57:27 -0700

    fix

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -12,12 +12,12 @@ char *_strcat(char *dest, char *src)
 {
 	while (*dest != '\0')
 	{
-		dest++;
+		*dest++;
 	}
 	while (*src != '\0')
 	{
 		*dest++ = *src++;
 	}
 	*dest = *dest + '\0';
-	return (*dest);
+	return (dest);
 }
