0aa1be9a68029dad976bda6eb754040b18290439
Jtownokie <6673@holbertonstudents.com>
2023-06-09 11:22:17 -0700

    strcmp function

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strcmp - Function that compares strings
+ * @s1: String Input
+ * @s2: String Input
+ *
+ * Return: +, -, or 0 Integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i = 0;
+	int result;
+
+	while (*s1)
+	{
+		if (s1[i] < s2[i])
+		{
+			result = -15;
+		}
+		else if (s1[i] > s2[i])
+		{
+			result = 15;
+		}
+		else if (s1[i] == s2[i])
+		{
+			result = 0;
+			s2++;
+		}
+		s1++;
+		i++;
+	}
+	return (result);
+}
