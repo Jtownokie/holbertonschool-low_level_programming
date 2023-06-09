dc8d40705e283ea7e01685406ddefd1766b431d9
Jtownokie <6673@holbertonstudents.com>
2023-06-09 12:21:29 -0700

    fix

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -9,18 +9,26 @@
 
 char *string_toupper(char *str)
 {
-	int i = 0;
+	int i;
+	int len = 0;
 	char *temp;
 
 	temp = str;
 
-	while (*str != '\0')
+	while (*str)
+	{
+		len++;
+	}
+
+	len = len - 1;
+
+	for (i = 0; i < len; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
 			str[i] = str[i] - 'a' + 'A';
 		}
-		i++;
 	}
+
 	return (temp);
 }
