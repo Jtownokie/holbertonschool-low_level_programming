b38d479d6870200f6b5579f8f6c46e4051411e70
Jtownokie <6673@holbertonstudents.com>
2023-06-08 18:34:39 -0700

    fix

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -12,6 +12,7 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+	char *temp = dest;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
@@ -22,5 +23,5 @@ char *_strncpy(char *dest, char *src, int n)
 		dest[i] = '\0';
 	}
 
-	return (dest);
+	return (temp);
 }
