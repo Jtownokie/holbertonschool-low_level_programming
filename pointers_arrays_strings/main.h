2a7cf8c848d0e2398c6b45cb77019893ee3b48f2
Jtownokie <6673@holbertonstudents.com>
2023-06-11 12:45:37 -0700

    Memset Function

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -61,3 +61,27 @@ char *cap_string(char *);
 
 /* Leet Code Function */
 char *leet(char *);
+
+/* Memset Function */
+char *_memset(char *s, char b, unsigned int n);
+
+/* Memcpy Function */
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+/* Strchr Function */
+char *_strchr(char *s, char c);
+
+/* Strspn Function */
+unsigned int _strspn(char *s, char *accept);
+
+/* Strpbrk Function */
+char *_strpbrk(char *s, char *accept);
+
+/* Strstr Function */
+char *_strstr(char *haystack, char *needle);
+
+/* Chessboard Function */
+void print_chessboard(char (*a)[8]);
+
+/* Print Diagonal Sum Function */
+void print_diagsums(int *a, int size);
