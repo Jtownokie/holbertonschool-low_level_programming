c3fc181feaf52022505b474ede859d79178dfd1f
Jtownokie <6673@holbertonstudents.com>
2023-06-11 20:34:26 -0700

    Print Diagonal Sums

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_diagsums - Prints sum of both diagonals
+ * @a: array of size 'size'
+ * @size: size of array
+ *
+ * Return: Always void
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int i, sum1, sum2;
+
+	for (i = 0; i < size; i++)
+	{
+		sum1 = sum1 + a[i][i];
+		sum2 = sum2 + a[i][size - i - 1];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
