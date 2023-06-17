2237a561027fc7e19757435b59728037fa9256aa
Jtownokie <6673@holbertonstudents.com>
2023-06-17 15:03:07 -0700

    fix

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (!(isdigit(argv[i])))
+			if (!(isdigit(atoi(argv[i]))))
 			{
 				printf("Error\n");
 				return (1);
