f58a34809b739e3679a850fd62256a8ebf986919
Jtownokie <6673@holbertonstudents.com>
2023-06-17 13:28:51 -0700

    Multiply Arguments Program

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * main - Program that prints all arguments passed to it
+ * @argc: Number of arguments passed to program
+ * @argv: Array of string arguments passed to program
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, result;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		result = 1;
+		for (i = 1; i < argc; i++)
+		{
+			result *= atoi(argv[i]);
+		}
+		printf("%d\n", result);
+	}
+
+	return (0);
+}
