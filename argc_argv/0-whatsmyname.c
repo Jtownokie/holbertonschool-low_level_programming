129a5414f1e70f2608fe7d7d48f95886abd13c1e
Jtownokie <6673@holbertonstudents.com>
2023-06-17 13:03:00 -0700

    Program that prints its own name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - Program that prints its' own name
+ * @argc: Number of arguments passed to program
+ * @argv: Array of string arguments passed to program
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[]);
+{
+	printf("%s", argv[0]);
+	return (0);
+}
