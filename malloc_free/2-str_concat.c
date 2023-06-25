37d31c8fbebe7ca4eab6a501ad5f8aebda35b05b
Jtownokie <6673@holbertonstudents.com>
2023-06-25 12:02:14 -0700

    fix

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -16,12 +16,12 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		*s1 = "";
+		s1 = "";
 	}
 
 	if (s2 == NULL)
 	{
-		*s2 = "";
+		s2 = "";
 	}
 
 	new_string = malloc(((_strlen(s1) + _strlen(s2)) + 1) * sizeof(char));
