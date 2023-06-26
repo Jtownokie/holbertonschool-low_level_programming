ca80ff5ceef2bf679f4caec97a488ebb63db9403
Jtownokie <6673@holbertonstudents.com>
2023-06-25 20:16:13 -0700

    fix

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -15,13 +15,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int i, j, k;
 	char *new_string;
 
-	k = _strlen(s2);
-
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
+
+	k = _strlen(s2);
 
 	if (n >= k)
 		new_string = malloc(((_strlen(s1) + _strlen(s2)) + 1) * sizeof(char));
