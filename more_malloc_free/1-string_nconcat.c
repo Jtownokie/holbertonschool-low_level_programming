66397cf09f931a1390b64635cd6b387ba03a319d
Jtownokie <6673@holbertonstudents.com>
2023-06-25 20:06:23 -0700

    String_Nconcat Function

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,49 +2,45 @@
 #include "strlen.c"
 
 /**
- * *str_concat - Function that returns a pointer to concatenated string
+ * *string_nconcat - Function that returns a pointer to concatenated string
  * @s1: Source String
  * @s2: String to Concatenate
+ * @n: Number of bytes to add
  *
  * Return: Pointer to the new mem space, or NULL if str is NULL or if it fails
  */
 
-char *str_concat(char *s1, char *s2)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	int i;
+	int i, j;
 	char *new_string;
 
 	if (s1 == NULL)
-	{
 		s1 = "";
-	}
 
 	if (s2 == NULL)
-	{
 		s2 = "";
-	}
 
-	new_string = malloc(((_strlen(s1) + _strlen(s2)) + 1) * sizeof(char));
+	if (n >= _strlen(s2))
+		new_string = malloc(((_strlen(s1) + _strlen(s2)) + 1) * sizeof(char));
+	else
+		new_string = malloc(((_strlen(s1) + n) + 1) * sizeof(char));
 
 	if (new_string == NULL)
-	{
 		return (NULL);
-	}
 
 	i = 0;
 	while (*s1 != '\0')
-	{
 		new_string[i] = *s1;
 		s1++;
 		i++;
-	}
 
-	while (*s2 != '\0')
-	{
+	j = 0;
+	while (*s2 != '\0' && j < n)
 		new_string[i] = *s2;
 		s2++;
 		i++;
-	}
+		j++;
 	new_string[i] = '\0';
 
 	return (new_string);
