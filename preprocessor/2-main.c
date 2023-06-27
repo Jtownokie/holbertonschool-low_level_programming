b78359dce65cc641c85e3defb1ac7c26d16bc377
Jtownokie <6673@holbertonstudents.com>
2023-06-26 17:50:36 -0700

    Print Source File

---
+++ preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program that prints the file it was compiled from
+ *
+ * Return: Always Void
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
