a7b4d0275bf516fa167e3dbf81d92711d47ce84e
Jtownokie <6673@holbertonstudents.com>
2023-06-26 17:58:53 -0700

    Tasks 3 & 4

---
+++ preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_LIKE_MACRO_H
+#define FUNCTION_LIKE_MACRO_H
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define ABS(x) ((x < 0) ? (-x) : (x))
+
+#endif
---
+++ preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef SUM_H
+#define SUM_H
+
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define SUM(x, y) (x + y)
+
+#endif
