091b8fe0b16d4cda9f9745945b894470d79f3f31
Jtownokie <6673@holbertonstudents.com>
2023-07-01 05:32:34 -0700

    fix

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -17,18 +17,18 @@ int main(int argc, char **argv)
 		printf("Error\n");
 		exit(98);
 	}
-	if (strchr("+-*/%", argv[2]) == NULL)
+	if (strchr("+-*/%", *argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((argv[2] == '/' || argv[2] == '%') && (argv[3] == '0'))
+	if ((*argv[2] == '/' || *argv[2] == '%') && (*argv[3] == '0'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+	result = get_op_func(*argv[2])(atoi(*argv[1]), atoi(*argv[3]));
 
 	printf("%d\n", result);
 
