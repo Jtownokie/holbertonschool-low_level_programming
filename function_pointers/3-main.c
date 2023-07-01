ec9ec34f94b00f48007afb34af9e19c2e2d8ed96
Jtownokie <6673@holbertonstudents.com>
2023-07-01 06:32:10 -0700

    fix

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -11,35 +11,34 @@
 int main(int argc, char **argv)
 {
 	char *getop;
-	int num1;
-	int num2;
+	int num1, num2, result;
 	int (*opfunc)(int, int);
-	int result;
-
-	getop = *argv[2];
-	num1 = atoi(*argv[1]);
-	num2 = atoi(*argv[3]);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	if (strchr("+-*/%", *getop) == NULL)
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[3]);
+	getop = argv[2];
+
+	if (get_op_func(argv[2]) == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*getop == '/' || *getop == '%') && (num2 == 0))
+	if ((*getop == '/' || *getop == '%') && (*argv[3] == '0'))
 	{
 		printf("Error\n");
 		exit(100);
 	}
 
-	opfunc = get_op_func(*getop);
+	opfunc = get_op_func(getop);
 	result = opfunc(num1, num2);
 
 	printf("%d\n", result);
 
-	return (0);
+	return (0);;
 }
