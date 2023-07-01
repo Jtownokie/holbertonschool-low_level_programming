1fe2f4748f5e0e340559c1e4dbaecbab2f00e73b
Jtownokie <6673@holbertonstudents.com>
2023-07-01 05:23:38 -0700

    Simple Calc Program

---
+++ function_pointers/3-main.c
@@ -0,0 +1,36 @@
+#include "3-calc.h"
+
+/**
+ * main - Main Function of Calc Program
+ * @argc: Num arguments
+ * @argv: Array of argument strings
+ *
+ * Return: Always void
+ */
+
+int main(int argc, char **argv)
+{
+	int result;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (strchr("+-*/%", argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if ((argv[2] == '/' || argv[2] == '%') && (argv[3] == '0'))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	result = get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3]));
+
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Returns the sum of a and b
+ * @a: Int 1
+ * @b: Int 2
+ *
+ * Return: Sum of a and b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Returns the difference of a and b
+ * @a: Int 1
+ * @b: Int 2
+ *
+ * Return: Difference of a and b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - Returns the product of a and b
+ * @a: Int 1
+ * @b: Int 2
+ *
+ * Return: Product of a and b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Returns the division of a and b
+ * @a: Int 1
+ * @b: Int 2
+ *
+ * Return: Division of a and b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns the mod of a and b
+ * @a: Int 1
+ * @b: Int 2
+ *
+ * Return: Mod of a and b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
