5e185bf391c92c6dab0e9c5907befdf44a9b4516
Jtownokie <6673@holbertonstudents.com>
2023-06-30 19:01:46 -0700

    Array Action Function Pointer

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Function that executes a function on array
+ * @array: Array to execute function on
+ * @size: Size of Array
+ * @action: Pointer to functions
+ *
+ * Return: Always Void
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+{
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
