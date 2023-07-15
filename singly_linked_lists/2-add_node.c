44f20b0fd8bd04fb2aeae8a7ac41901bfcc08051
Jtownokie <6673@holbertonstudents.com>
2023-07-14 20:24:50 -0700

    Added string length, corrected node pointer syntax

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -11,7 +11,12 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	
+	unsigned int i = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -19,10 +24,16 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+
 	new_node->str = strdup(str);
+	new_node->len = i;
 	new_node->next = *head;
 
-	head = new_node;
+	*head = new_node;
 
-	return (head);
+	return (new_node);
 }
