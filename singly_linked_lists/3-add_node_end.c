eff886e0cd7a09de62eed0b54a4aae8829dbec2a
Jtownokie <6673@holbertonstudents.com>
2023-07-14 20:33:52 -0700

    Fix, added temp pointer

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -11,12 +11,8 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node;
+	list_t *temp_node;
 	unsigned int i = 0;
-
-	if (head == NULL)
-	{
-		return (NULL);
-	}
 
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
@@ -33,7 +29,20 @@ list_t *add_node_end(list_t **head, const char *str)
 	new_node->len = i;
 	new_node->next = NULL;
 
-	*head = new_node;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		return (new_node);
+	}
+
+	temp = *head;
+
+	while (temp->next != NULL)
+	{
+		temp = temp->next;
+	}
+
+	temp->next = new;
 
 	return (new_node);
 }
