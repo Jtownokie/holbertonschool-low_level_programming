9276671471d8e1aa762997fc778c131b6ee67b56
Jtownokie <6673@holbertonstudents.com>
2023-07-16 13:36:18 -0700

    Function that Reads/Prints a Text File

---
+++ file_io/1-create_file.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * create_file - Function that creates a file
+ * @filename: Name of File
+ * @text_content: Content of File
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+}
---
+++ file_io/2-append_text_to_file.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Function that appends text to file
+ * @filename: File to add text to
+ * @text_content: Text to add
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+}
