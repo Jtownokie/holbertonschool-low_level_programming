4a777ec75ab25b9f512ca58cddecefe932ee1c72
Jtownokie <6673@holbertonstudents.com>
2023-07-19 14:04:32 -0700

    Fix to code order

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -13,20 +13,23 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	dlistint_t *last;
 
+	if (head == NULL)
+		return (NULL);
+
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 
-	last = *head;
-
 	new_node->n = n;
 	new_node->next = NULL;
 
+	last = *head;
+
 	if (*head != NULL)
 	{
-		new_node->prev = NULL;
 		*head = new_node;
+		new_node->prev = NULL;
 		return (new_node);
 	}
 
