f15ebeb150ea95b1584a33fc0cd3272707fc2920
Jtownokie <6673@holbertonstudents.com>
2023-07-19 20:40:16 -0700

    Function restructure with for loop, added edge cases

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -13,32 +13,42 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	dlistint_t *current;
 	dlistint_t *new_node;
-	unsigned int node_at_index = 0;
+	unsigned int i;
 
 	if (h == NULL)
 		return (NULL);
 
 	new_node = malloc(sizeof(dlistint_t));
-
 	if (new_node == NULL)
 		return (NULL);
 
-	new_node->n = n;
 	current = *h;
-
-	while (current->next != NULL && node_at_index != idx)
+	new_node->n = n;
+	new_node->next = NULL;
+	new_node->prev = NULL;
+	if (idx == 0)
+	{
+		if (*h == NULL)
+			(*h) = new_node;
+		else
+		{
+			new_node->next = *h;
+			current->prev = new_node;
+			*h = new_node;
+		}
+		return (new_node);
+	}
+	for (i = 0; i < (idx - 1); i++)
 	{
 		current = current->next;
-		node_at_index++;
+		if (current == NULL)
+			return (NULL);
 	}
-
-	if (node_at_index != idx)
-		return (NULL);
-
-	new_node->next = current;
-	new_node->prev = current->prev;
-	current->prev->next = new_node;
-	current->prev = new_node;
-
+	new_node->n = n;
+	new_node->next = current->next;
+	new_node->prev = current;
+	if (current->next != NULL)
+		current->next->prev = new_node;
+	current->next = newnode;
 	return (new_node);
 }
