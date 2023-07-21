85b94f2764be3936191a512c00f6a5fe96ec0233
Jtownokie <6673@holbertonstudents.com>
2023-07-21 14:14:23 -0700

    Betty Correction

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -12,7 +12,7 @@ void hash_table_delete(hash_table_t *ht)
 	hash_node_t *current, *next;
 	unsigned long int index;
 
-	if(!ht)
+	if (!ht)
 		return;
 	for (index = 0; index < ht->size; index++)
 	{
