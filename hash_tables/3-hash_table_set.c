3fbfeeeee9f4723f6e12b32f4f8ec2fa707f364f
Jtownokie <6673@holbertonstudents.com>
2023-07-21 13:32:59 -0700

    Syntax Correction

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,8 +14,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index = 0;
 	hash_node_t *hash_node, *new_node;
 
-	if (!ht || !ht->array || !value)
-		return(0);
+	if (!ht || !ht->array)
+		return (0);
 
 	if (*key == '\0' || strlen(key) == 0)
 		return (0);
@@ -28,7 +28,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(hash_node->key, key) == 0)
 		{
-			free(hash_node->value);
 			hash_node->value = strdup(value);
 			return (1);
 		}
