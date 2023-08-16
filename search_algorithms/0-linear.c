3ed1425ab1597b396fd69f017ba040c8a188142d
Jtownokie <6673@holbertonstudents.com>
2023-08-16 12:26:29 -0700

    Created Directory, submitting linear search algorithm project

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,29 @@
+#include "search_algos.h"
+
+/**
+* linear_search - Linear search algorithm
+* @array: Pointer to array
+* @size: Size of the array
+* @value: Value to search for
+*
+* Return: 0 if success or else -1
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			printf("Found %d at index: %d\n", array[i], i);
+			return (i);
+		}
+	}
+	return (-1);
+}
